0cf1daad9849a866b4c03c7bd03ae99f9993a247
dbalmain <dbalmain@06fd6eb0-0002-0410-a719-e5602cce40bc>
2007-02-28 19:54:40 +0000

    removed -fno-stack-protector option
    git-svn-id: svn+ssh://davebalmain.com/home/dave/repos/ferret/trunk@738 06fd6eb0-0002-0410-a719-e5602cce40bc

--- ruby/ext/extconf.rb
+++ ruby/ext/extconf.rb
@@ -3,9 +3,12 @@ if (/mswin/ =~ RUBY_PLATFORM) and ENV['make'].nil?
   require 'mkmf'
   $LIBS += " msvcprt.lib"
   create_makefile("ferret_ext")
+elsif ENV['FERRET_DEV']
+  require 'mkmf'
+  $CFLAGS += " -fno-stack-protector -fno-common -D_FILE_OFFSET_BITS=64"
+  create_makefile("ferret_ext")
 else
   require 'mkmf'
-  #$CFLAGS += " -fno-common"
-  $CFLAGS += " -fno-stack-protector -fno-common -D_FILE_OFFSET_BITS=64"
+  $CFLAGS += " -D_FILE_OFFSET_BITS=64"
   create_makefile("ferret_ext")
 end
