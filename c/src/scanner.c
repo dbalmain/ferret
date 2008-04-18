2ddc5a3769128e78858a38f000f37eba179f08e1
dave <dave@06fd6eb0-0002-0410-a719-e5602cce40bc>
2008-04-18 09:11:35 +0000

    Added scanner.c to repository
    Although scanner.c will be generated by ragel during the build process, it is
    important to include the generated code for those who don't have ragel
    installed. The generated code should be portable so this isn't an issue.
    
    
    git-svn-id: svn+ssh://davebalmain.com/home/dave/repos/ferret/trunk@958 06fd6eb0-0002-0410-a719-e5602cce40bc

---
+++ c/src/scanner.c
@@ -0,0 +1,497 @@
+#line 1 "src/scanner.rl"
+/* scanner.rl -*-C-*- */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define RET do {                                \
+    size_t __len = te - ts - skip - trunc;      \
+    if (__len > out_size)                       \
+        __len = out_size;                       \
+    *len     = __len;                           \
+    *start   = ts;                              \
+    *end     = te;                              \
+     memcpy(out, ts + skip, __len);             \
+     out[__len] = 0;                            \
+     return;                                    \
+} while(0)
+
+#define STRIP(c) do {                           \
+    char *__p = ts;                             \
+    char *__o = out;                            \
+    char *__max = __p + out_size;               \
+    for (; __p <= te && __p < __max; ++__p) {   \
+        if (*__p != c)                          \
+            *__o++ = *__p;                      \
+    }                                           \
+    *__o = 0;                                   \
+                                                \
+    *start = ts;                                \
+    *end   = te;                                \
+    *len   = __o - out;                         \
+    return;                                     \
+} while(0)
+
+#line 78 "src/scanner.rl"
+
+
+
+#line 41 "src/scanner.c"
+static const char _StdTok_actions[] = {
+	0, 1, 2, 1, 3, 1, 14, 1, 
+	15, 1, 16, 1, 17, 1, 18, 1, 
+	19, 1, 20, 1, 21, 1, 22, 1, 
+	23, 1, 24, 1, 25, 1, 26, 1, 
+	27, 1, 28, 2, 4, 5, 2, 4, 
+	6, 2, 4, 7, 2, 4, 8, 2, 
+	4, 9, 2, 4, 10, 2, 4, 11, 
+	2, 4, 12, 2, 4, 13, 4, 4, 
+	0, 1, 9, 4, 4, 0, 8, 1
+	
+};
+
+static const short _StdTok_key_offsets[] = {
+	0, 2, 8, 9, 16, 22, 29, 33, 
+	37, 38, 46, 57, 59, 62, 64, 74, 
+	81, 89, 93, 102, 114, 118, 124, 126, 
+	127, 138, 144, 151, 165, 177, 189, 200, 
+	208, 216, 228, 241, 253, 265
+};
+
+static const char _StdTok_trans_keys[] = {
+	48, 57, 48, 57, 65, 90, 97, 122, 
+	47, 95, 44, 58, 64, 90, 97, 122, 
+	48, 57, 65, 90, 97, 122, 46, 48, 
+	57, 65, 90, 97, 122, 65, 90, 97, 
+	122, 65, 90, 97, 122, 47, 47, 95, 
+	44, 58, 64, 90, 97, 122, 0, 43, 
+	45, 102, 104, 48, 57, 65, 90, 97, 
+	122, 48, 57, 46, 48, 57, 48, 57, 
+	45, 46, 58, 64, 48, 57, 65, 90, 
+	97, 122, 45, 48, 57, 65, 90, 97, 
+	122, 47, 95, 44, 58, 64, 90, 97, 
+	122, 65, 90, 97, 122, 45, 58, 64, 
+	48, 57, 65, 90, 97, 122, 38, 39, 
+	45, 46, 58, 64, 48, 57, 65, 90, 
+	97, 122, 65, 90, 97, 122, 48, 57, 
+	65, 90, 97, 122, 83, 115, 46, 38, 
+	39, 45, 58, 64, 48, 57, 65, 90, 
+	97, 122, 48, 57, 65, 90, 97, 122, 
+	46, 48, 57, 65, 90, 97, 122, 38, 
+	39, 45, 46, 58, 64, 105, 116, 48, 
+	57, 65, 90, 97, 122, 38, 39, 45, 
+	58, 64, 108, 48, 57, 65, 90, 97, 
+	122, 38, 39, 45, 58, 64, 101, 48, 
+	57, 65, 90, 97, 122, 38, 39, 45, 
+	58, 64, 48, 57, 65, 90, 97, 122, 
+	47, 95, 44, 58, 64, 90, 97, 122, 
+	47, 95, 44, 58, 64, 90, 97, 122, 
+	38, 39, 45, 58, 64, 112, 48, 57, 
+	65, 90, 97, 122, 38, 39, 45, 46, 
+	58, 64, 116, 48, 57, 65, 90, 97, 
+	122, 38, 39, 45, 58, 64, 116, 48, 
+	57, 65, 90, 97, 122, 38, 39, 45, 
+	58, 64, 112, 48, 57, 65, 90, 97, 
+	122, 38, 39, 45, 58, 64, 115, 48, 
+	57, 65, 90, 97, 122, 0
+};
+
+static const char _StdTok_single_lengths[] = {
+	0, 0, 1, 1, 0, 1, 0, 0, 
+	1, 2, 5, 0, 1, 0, 4, 1, 
+	2, 0, 3, 6, 0, 0, 2, 1, 
+	5, 0, 1, 8, 6, 6, 5, 2, 
+	2, 6, 7, 6, 6, 6
+};
+
+static const char _StdTok_range_lengths[] = {
+	1, 3, 0, 3, 3, 3, 2, 2, 
+	0, 3, 3, 1, 1, 1, 3, 3, 
+	3, 2, 3, 3, 2, 3, 0, 0, 
+	3, 3, 3, 3, 3, 3, 3, 3, 
+	3, 3, 3, 3, 3, 3
+};
+
+static const unsigned char _StdTok_index_offsets[] = {
+	0, 2, 6, 8, 13, 17, 22, 25, 
+	28, 30, 36, 45, 47, 50, 52, 60, 
+	65, 71, 74, 81, 91, 94, 98, 101, 
+	103, 112, 116, 121, 133, 143, 153, 162, 
+	168, 174, 184, 195, 205, 215
+};
+
+static const char _StdTok_indicies[] = {
+	1, 0, 2, 2, 2, 0, 3, 0, 
+	4, 4, 4, 4, 0, 6, 6, 6, 
+	5, 7, 6, 6, 6, 0, 8, 8, 
+	0, 9, 9, 0, 11, 10, 13, 12, 
+	12, 12, 12, 10, 15, 16, 16, 19, 
+	20, 17, 18, 18, 14, 22, 21, 24, 
+	22, 23, 1, 25, 27, 24, 28, 29, 
+	17, 30, 30, 26, 27, 2, 2, 2, 
+	26, 31, 4, 4, 4, 4, 0, 8, 
+	8, 32, 27, 28, 29, 30, 30, 30, 
+	26, 34, 35, 27, 36, 28, 38, 37, 
+	37, 37, 33, 40, 40, 39, 40, 40, 
+	40, 39, 42, 42, 41, 36, 43, 34, 
+	35, 27, 28, 38, 37, 37, 37, 33, 
+	6, 44, 44, 39, 7, 44, 44, 44, 
+	39, 34, 35, 27, 36, 28, 38, 45, 
+	46, 37, 37, 37, 33, 34, 35, 27, 
+	28, 38, 47, 37, 37, 37, 33, 34, 
+	35, 27, 28, 38, 48, 37, 37, 37, 
+	33, 34, 35, 27, 49, 38, 37, 37, 
+	37, 33, 51, 50, 50, 50, 50, 0, 
+	52, 12, 12, 12, 12, 0, 34, 35, 
+	27, 28, 38, 48, 37, 37, 37, 33, 
+	34, 35, 27, 36, 28, 38, 53, 37, 
+	37, 37, 33, 34, 35, 27, 28, 38, 
+	54, 37, 37, 37, 33, 34, 35, 27, 
+	28, 38, 55, 37, 37, 37, 33, 34, 
+	35, 27, 49, 38, 48, 37, 37, 37, 
+	33, 0
+};
+
+static const char _StdTok_trans_targs_wi[] = {
+	10, 13, 15, 3, 16, 10, 5, 6, 
+	17, 23, 10, 9, 31, 32, 10, 10, 
+	11, 14, 19, 27, 34, 10, 12, 10, 
+	0, 10, 10, 1, 2, 4, 18, 16, 
+	10, 10, 20, 22, 7, 24, 25, 10, 
+	21, 10, 10, 10, 26, 28, 33, 29, 
+	30, 8, 31, 31, 32, 35, 36, 37
+};
+
+static const char _StdTok_trans_actions_wi[] = {
+	33, 0, 38, 0, 53, 31, 0, 0, 
+	0, 56, 29, 0, 62, 62, 9, 7, 
+	0, 38, 35, 35, 35, 27, 59, 23, 
+	0, 25, 15, 0, 0, 0, 38, 50, 
+	19, 11, 0, 0, 0, 35, 41, 17, 
+	0, 13, 5, 21, 41, 35, 35, 35, 
+	35, 0, 47, 44, 67, 35, 35, 35
+};
+
+static const char _StdTok_to_state_actions[] = {
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 1, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0
+};
+
+static const char _StdTok_from_state_actions[] = {
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 3, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0
+};
+
+static const char _StdTok_eof_trans[] = {
+	1, 1, 1, 1, 6, 1, 1, 1, 
+	11, 11, 0, 22, 24, 26, 27, 27, 
+	1, 33, 27, 34, 40, 40, 42, 44, 
+	34, 40, 40, 34, 34, 34, 34, 1, 
+	1, 34, 34, 34, 34, 34
+};
+
+static const int StdTok_start = 10;
+static const int StdTok_error = -1;
+
+static const int StdTok_en_main = 10;
+
+#line 81 "src/scanner.rl"
+
+void frt_scan(const char *in,
+              char *out, size_t out_size,
+              char **start, char **end,
+              int *len)
+{
+    int cs, act;
+    char *ts, *te = 0;
+
+    
+#line 217 "src/scanner.c"
+	{
+	cs = StdTok_start;
+	ts = 0;
+	te = 0;
+	act = 0;
+	}
+#line 91 "src/scanner.rl"
+
+    char *p = (char *)in;
+    char *pe = 0;
+    char *eof = pe;
+    *len = 0;
+    int skip = 0;
+    int trunc = 0;
+
+    
+#line 234 "src/scanner.c"
+	{
+	int _klen;
+	unsigned int _trans;
+	const char *_acts;
+	unsigned int _nacts;
+	const char *_keys;
+
+	if ( p == pe )
+		goto _test_eof;
+_resume:
+	_acts = _StdTok_actions + _StdTok_from_state_actions[cs];
+	_nacts = (unsigned int) *_acts++;
+	while ( _nacts-- > 0 ) {
+		switch ( *_acts++ ) {
+	case 3:
+#line 1 "src/scanner.rl"
+	{ts = p;}
+	break;
+#line 253 "src/scanner.c"
+		}
+	}
+
+	_keys = _StdTok_trans_keys + _StdTok_key_offsets[cs];
+	_trans = _StdTok_index_offsets[cs];
+
+	_klen = _StdTok_single_lengths[cs];
+	if ( _klen > 0 ) {
+		const char *_lower = _keys;
+		const char *_mid;
+		const char *_upper = _keys + _klen - 1;
+		while (1) {
+			if ( _upper < _lower )
+				break;
+
+			_mid = _lower + ((_upper-_lower) >> 1);
+			if ( (*p) < *_mid )
+				_upper = _mid - 1;
+			else if ( (*p) > *_mid )
+				_lower = _mid + 1;
+			else {
+				_trans += (_mid - _keys);
+				goto _match;
+			}
+		}
+		_keys += _klen;
+		_trans += _klen;
+	}
+
+	_klen = _StdTok_range_lengths[cs];
+	if ( _klen > 0 ) {
+		const char *_lower = _keys;
+		const char *_mid;
+		const char *_upper = _keys + (_klen<<1) - 2;
+		while (1) {
+			if ( _upper < _lower )
+				break;
+
+			_mid = _lower + (((_upper-_lower) >> 1) & ~1);
+			if ( (*p) < _mid[0] )
+				_upper = _mid - 2;
+			else if ( (*p) > _mid[1] )
+				_lower = _mid + 2;
+			else {
+				_trans += ((_mid - _keys)>>1);
+				goto _match;
+			}
+		}
+		_trans += _klen;
+	}
+
+_match:
+	_trans = _StdTok_indicies[_trans];
+_eof_trans:
+	cs = _StdTok_trans_targs_wi[_trans];
+
+	if ( _StdTok_trans_actions_wi[_trans] == 0 )
+		goto _again;
+
+	_acts = _StdTok_actions + _StdTok_trans_actions_wi[_trans];
+	_nacts = (unsigned int) *_acts++;
+	while ( _nacts-- > 0 )
+	{
+		switch ( *_acts++ )
+		{
+	case 0:
+#line 58 "src/scanner.rl"
+	{ skip = p - ts; }
+	break;
+	case 1:
+#line 59 "src/scanner.rl"
+	{ skip = p - ts; }
+	break;
+	case 4:
+#line 1 "src/scanner.rl"
+	{te = p+1;}
+	break;
+	case 5:
+#line 47 "src/scanner.rl"
+	{act = 1;}
+	break;
+	case 6:
+#line 52 "src/scanner.rl"
+	{act = 4;}
+	break;
+	case 7:
+#line 55 "src/scanner.rl"
+	{act = 5;}
+	break;
+	case 8:
+#line 58 "src/scanner.rl"
+	{act = 6;}
+	break;
+	case 9:
+#line 59 "src/scanner.rl"
+	{act = 7;}
+	break;
+	case 10:
+#line 60 "src/scanner.rl"
+	{act = 8;}
+	break;
+	case 11:
+#line 61 "src/scanner.rl"
+	{act = 9;}
+	break;
+	case 12:
+#line 67 "src/scanner.rl"
+	{act = 11;}
+	break;
+	case 13:
+#line 70 "src/scanner.rl"
+	{act = 12;}
+	break;
+	case 14:
+#line 49 "src/scanner.rl"
+	{te = p+1;{ trunc = 2; RET; }}
+	break;
+	case 15:
+#line 74 "src/scanner.rl"
+	{te = p+1;{ return; }}
+	break;
+	case 16:
+#line 75 "src/scanner.rl"
+	{te = p+1;}
+	break;
+	case 17:
+#line 47 "src/scanner.rl"
+	{te = p;p--;{ RET; }}
+	break;
+	case 18:
+#line 48 "src/scanner.rl"
+	{te = p;p--;{ trunc = 1; RET; }}
+	break;
+	case 19:
+#line 52 "src/scanner.rl"
+	{te = p;p--;{ RET; }}
+	break;
+	case 20:
+#line 55 "src/scanner.rl"
+	{te = p;p--;{ RET; }}
+	break;
+	case 21:
+#line 64 "src/scanner.rl"
+	{te = p;p--;{ RET; }}
+	break;
+	case 22:
+#line 67 "src/scanner.rl"
+	{te = p;p--;{ STRIP('.'); }}
+	break;
+	case 23:
+#line 70 "src/scanner.rl"
+	{te = p;p--;{ RET; }}
+	break;
+	case 24:
+#line 71 "src/scanner.rl"
+	{te = p;p--;{ RET; }}
+	break;
+	case 25:
+#line 75 "src/scanner.rl"
+	{te = p;p--;}
+	break;
+	case 26:
+#line 47 "src/scanner.rl"
+	{{p = ((te))-1;}{ RET; }}
+	break;
+	case 27:
+#line 52 "src/scanner.rl"
+	{{p = ((te))-1;}{ RET; }}
+	break;
+	case 28:
+#line 1 "src/scanner.rl"
+	{	switch( act ) {
+	case 1:
+	{{p = ((te))-1;} RET; }
+	break;
+	case 4:
+	{{p = ((te))-1;} RET; }
+	break;
+	case 5:
+	{{p = ((te))-1;} RET; }
+	break;
+	case 6:
+	{{p = ((te))-1;} trunc = 1; RET; }
+	break;
+	case 7:
+	{{p = ((te))-1;} RET; }
+	break;
+	case 8:
+	{{p = ((te))-1;} trunc = 1; RET; }
+	break;
+	case 9:
+	{{p = ((te))-1;} RET; }
+	break;
+	case 11:
+	{{p = ((te))-1;} STRIP('.'); }
+	break;
+	case 12:
+	{{p = ((te))-1;} RET; }
+	break;
+	default: break;
+	}
+	}
+	break;
+#line 457 "src/scanner.c"
+		}
+	}
+
+_again:
+	_acts = _StdTok_actions + _StdTok_to_state_actions[cs];
+	_nacts = (unsigned int) *_acts++;
+	while ( _nacts-- > 0 ) {
+		switch ( *_acts++ ) {
+	case 2:
+#line 1 "src/scanner.rl"
+	{ts = 0;}
+	break;
+#line 470 "src/scanner.c"
+		}
+	}
+
+	if ( ++p != pe )
+		goto _resume;
+	_test_eof: {}
+	if ( p == eof )
+	{
+	if ( _StdTok_eof_trans[cs] > 0 ) {
+		_trans = _StdTok_eof_trans[cs] - 1;
+		goto _eof_trans;
+	}
+	}
+
+	}
+#line 100 "src/scanner.rl"
+
+    if ( cs == StdTok_error )
+    {
+        fprintf(stderr, "PARSE ERROR\n" );
+        return;
+    }
+
+    if ( ts )
+    {
+        fprintf(stderr, "STUFF LEFT: '%s'\n", ts);
+    }
+}
