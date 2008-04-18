fd1bb8742f294587b873cad7029e596a7fb0500e
dave <dave@06fd6eb0-0002-0410-a719-e5602cce40bc>
2008-04-18 09:11:35 +0000

    Automated adding of tests.
    Just declare each TestSuite like so;
    
        TestSuite *ts_<name of test suite>(TestSuite *suite)
    
    A rake task will automatically search the test files for new test suites and
    add them to the list to be run.
    
    Also removed the bench executables from the clean-task lists in the Makefiles.
    These are going to be moved to the bench suite.
    
    
    git-svn-id: svn+ssh://davebalmain.com/home/dave/repos/ferret/trunk@959 06fd6eb0-0002-0410-a719-e5602cce40bc

--- c/test/all_tests.h
+++ c/test/all_tests.h
@@ -3,81 +3,82 @@
 
 #include "test.h"
 
-extern TestSuite *ts_test(TestSuite *suite);
-extern TestSuite *ts_global(TestSuite *suite);
-extern TestSuite *ts_except(TestSuite *suite);
-extern TestSuite *ts_array(TestSuite *suite);
-extern TestSuite *ts_hash(TestSuite *suite);
-extern TestSuite *ts_hashset(TestSuite *suite);
-extern TestSuite *ts_bitvector(TestSuite *suite);
-extern TestSuite *ts_priorityqueue(TestSuite *suite);
-extern TestSuite *ts_helper(TestSuite *suite);
-extern TestSuite *ts_mem_pool(TestSuite *suite);
-extern TestSuite *ts_fs_store(TestSuite *suite);
-extern TestSuite *ts_ram_store(TestSuite *suite);
-extern TestSuite *ts_compound_io(TestSuite *suite);
-extern TestSuite *ts_fields(TestSuite *suite);
-extern TestSuite *ts_segments(TestSuite *suite);
-extern TestSuite *ts_document(TestSuite *suite);
-extern TestSuite *ts_multimapper(TestSuite *suite);
-extern TestSuite *ts_analysis(TestSuite *suite);
-extern TestSuite *ts_term(TestSuite *suite);
-extern TestSuite *ts_term_vectors(TestSuite *suite);
-extern TestSuite *ts_similarity(TestSuite *suite);
-extern TestSuite *ts_index(TestSuite *suite);
-extern TestSuite *ts_file_deleter(TestSuite *suite);
-extern TestSuite *ts_search(TestSuite *suite);
-extern TestSuite *ts_multi_search(TestSuite *suite);
-extern TestSuite *ts_q_fuzzy(TestSuite *suite);
-extern TestSuite *ts_q_filtered(TestSuite *suite);
-extern TestSuite *ts_q_span(TestSuite *suite);
-extern TestSuite *ts_q_const_score(TestSuite *suite);
-extern TestSuite *ts_filter(TestSuite *suite);
-extern TestSuite *ts_sort(TestSuite *suite);
-extern TestSuite *ts_q_parser(TestSuite *suite);
-extern TestSuite *ts_highlighter(TestSuite *suite);
-extern TestSuite *ts_threading(TestSuite *suite);
-extern TestSuite *ts_lang(TestSuite *suite);
+
+TestSuite *ts_fields(TestSuite *suite);
+TestSuite *ts_file_deleter(TestSuite *suite);
+TestSuite *ts_multimapper(TestSuite *suite);
+TestSuite *ts_q_filtered(TestSuite *suite);
+TestSuite *ts_filter(TestSuite *suite);
+TestSuite *ts_hashset(TestSuite *suite);
+TestSuite *ts_compound_io(TestSuite *suite);
+TestSuite *ts_document(TestSuite *suite);
+TestSuite *ts_global(TestSuite *suite);
+TestSuite *ts_hash(TestSuite *suite);
+TestSuite *ts_search(TestSuite *suite);
+TestSuite *ts_multi_search(TestSuite *suite);
+TestSuite *ts_lang(TestSuite *suite);
+TestSuite *ts_priorityqueue(TestSuite *suite);
+TestSuite *ts_q_parser(TestSuite *suite);
+TestSuite *ts_highlighter(TestSuite *suite);
+TestSuite *ts_q_fuzzy(TestSuite *suite);
+TestSuite *ts_fs_store(TestSuite *suite);
+TestSuite *ts_q_const_score(TestSuite *suite);
+TestSuite *ts_sort(TestSuite *suite);
+TestSuite *ts_term(TestSuite *suite);
+TestSuite *ts_test(TestSuite *suite);
+TestSuite *ts_q_span(TestSuite *suite);
+TestSuite *ts_segments(TestSuite *suite);
+TestSuite *ts_array(TestSuite *suite);
+TestSuite *ts_term_vectors(TestSuite *suite);
+TestSuite *ts_threading(TestSuite *suite);
+TestSuite *ts_ram_store(TestSuite *suite);
+TestSuite *ts_analysis(TestSuite *suite);
+TestSuite *ts_index(TestSuite *suite);
+TestSuite *ts_bitvector(TestSuite *suite);
+TestSuite *ts_except(TestSuite *suite);
+TestSuite *ts_mem_pool(TestSuite *suite);
+TestSuite *ts_similarity(TestSuite *suite);
+TestSuite *ts_helper(TestSuite *suite);
 
 const struct test_list
 {
     TestSuite *(*func)(TestSuite *suite);
 } all_tests[] = {
-    {ts_test},
-    {ts_global},
-    {ts_except},
-    {ts_array},
-    {ts_hash},
-    {ts_hashset},
-    {ts_bitvector},
-    {ts_priorityqueue},
-    {ts_helper},
-    {ts_mem_pool},
-    {ts_fs_store},
-    {ts_ram_store},
-    {ts_compound_io},
     {ts_fields},
-    {ts_segments},
-    {ts_document},
-    {ts_multimapper},
-    {ts_analysis},
-    {ts_term},
-    {ts_term_vectors},
-    {ts_similarity},
-    {ts_index},
     {ts_file_deleter},
+    {ts_multimapper},
+    {ts_q_filtered},
+    {ts_filter},
+    {ts_hashset},
+    {ts_compound_io},
+    {ts_document},
+    {ts_global},
+    {ts_hash},
     {ts_search},
     {ts_multi_search},
-    {ts_q_fuzzy},
-    {ts_q_filtered},
-    {ts_q_span},
-    {ts_q_const_score},
-    {ts_filter},
-    {ts_sort},
+    {ts_lang},
+    {ts_priorityqueue},
     {ts_q_parser},
     {ts_highlighter},
+    {ts_q_fuzzy},
+    {ts_fs_store},
+    {ts_q_const_score},
+    {ts_sort},
+    {ts_term},
+    {ts_test},
+    {ts_q_span},
+    {ts_segments},
+    {ts_array},
+    {ts_term_vectors},
     {ts_threading},
-    {ts_lang}
+    {ts_ram_store},
+    {ts_analysis},
+    {ts_index},
+    {ts_bitvector},
+    {ts_except},
+    {ts_mem_pool},
+    {ts_similarity},
+    {ts_helper}
 };
 
 #endif
